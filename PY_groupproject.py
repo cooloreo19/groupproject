d722b0f06d13cfa43e9d9e7e348b35998c936bf0
Nosawaru Efemwonkieke <nosawaruefemwonkieke@gmail.com>
2019-07-29 09:54:20 -0700

    Add webapp and jinja

---
+++ PY_groupproject.py
@@ -0,0 +1,17 @@
+import webapp2
+import jinja2
+import os
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Saluton, Mondo!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage)
+], debug=True)
