0fc300544e83144680569fb82135f8c64843944a
Hunter <hsch3m3ng01@gmail.com>
2019-08-01 10:38:13 -0700

    IMPORTANT IMPORTANT

--- brainquizmain.py
+++ brainquizmain.py
@@ -17,43 +17,91 @@ class MainPage(webapp2.RequestHandler):
         self.response.write(home_template.render())
 
 class BrainQuizPage(webapp2.RequestHandler):
+
+
+
     def get(self):
+        current = self.request.get("current")
+        next = 0
+        if current != "":
+            next = int(current)+ 1
+
+
 
         trivia_url_endpoint = [
-        {"ID": 0, "question": "FRONTAL LOBE: dlfjldkfglgkjdflgkjfdlgkjld?",
+        {"ID": 0, "question": "Imagine yourself in a dungeon. There are two doors: One you know for sure is the way out, and the other contains a furocious lion that hasn't eaten in three weeks! What path will you choose?",
             "count": 1,
-            "correct_answer": "Jupiter",
+            "correct_answer": "Door 1",
             "answers": [
-                "Jupiter",
-                "Uranus",
-                "Neptune",
-                "Mars"
+                "Door 1",
+                "Door 2",
+                "Sit and Rot",
+                "Make Tallies on the Wall"
             ]
         },
-        {"ID":1, "question": "TEMPORAL LOBE: ",
-            "count": 1,
-            "correct_answer": "Jiter",
+        {"ID":1, "question": "You friend gives you this wierd looking phrase. Can you figur out what letters come next? :: Y F G Y T W L P C Y F O ...",
+           "count": 1,
+            "correct_answer": "W L C N",
             "answers": [
-                "Jiter",
-                "Urus",
-                "Nune",
-                "Mrs"
+                "W Q D N",
+                "J I D K",
+                "W L C N",
+                "I D C N"
                 ]
             },
-        ]
+        {"ID":2, "question": "You friend gives you this wierd looking phrase. Can you figur out what letters come next? :: Y F G Y T W L P C Y F O ...",
+           "count": 1,
+            "correct_answer": "W L C N",
+            "answers": [
+                "W Q D N",
+                "J I D K",
+                "W L C N",
+                "I D C N"
+            ]
+        },
+        {"ID":3, "question": "You friend gives you this wierd looking phrase. Can you figur out what letters come next? :: Y F G Y T W L P C Y F O ...",
+           "count": 1,
+            "correct_answer": "W L C N",
+            "answers": [
+                "W Q D N",
+                "J I D K",
+                "W L C N",
+                "I D C N"
+            ]
+        },
+        {"ID":4, "question": "You friend gives you this wierd looking phrase. Can you figur out what letters come next? :: Y F G Y T W L P C Y F O ...",
+           "count": 1,
+            "correct_answer": "W L C N",
+            "answers": [
+                "W Q D N",
+                "J I D K",
+                "W L C N",
+                "I D C N"
+            ]
+        },
+    ]
+        if next >= len(trivia_url_endpoint):
+            self.response.write("<meta http-equiv=\"Refresh\" content=\"0; url=https://www.w3docs.com\" />")
+            return
+
+
+
 
 
         quiz_template = the_jinja_env.get_template('quiz.html')
 
 
 
-        all_answers = trivia_url_endpoint[0]["answers"]
+        # all_answers = trivia_url_endpoint[next]
+        # for trivia_url_endpoint.next in trivia_url_endpoint:
+        #     all_answers.append(trivia_url_endpoint.next["answers"])
+        # add ...........................^[next]
         # for answer in trivia_url_endpoint[0]["incorrect_answers"]:
-        #     all_answers.append(answer)
-        random.shuffle(all_answers)
 
-        qtn = { "answers": all_answers,
-            "question": trivia_url_endpoint[0]["question"], "correct": trivia_url_endpoint[0]["correct_answer"]
+        # random.shuffle(all_answers)
+
+        qtn = { "answers": trivia_url_endpoint[next]["answers"],
+            "question": trivia_url_endpoint[next]["question"], "correct": trivia_url_endpoint[next]["correct_answer"], "ID": next
             }
 
 
