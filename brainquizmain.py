4b4a719b2fbc1a3d0ef71ce08b200d8671dd89d1
cssi <cssi@cssis-MacBook-Pro.local>
2019-08-01 17:35:33 -0700

    finally did it

--- brainquizmain.py
+++ brainquizmain.py
@@ -1,7 +1,8 @@
 import webapp2
+from google.appengine.api import users
+from google.appengine.ext import ndb
 import jinja2
 import os
-# from google.appengine.api import urlfetch
 import json
 import random
 
@@ -11,6 +12,83 @@ the_jinja_env = jinja2.Environment(
 loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
 extensions=['jinja2.ext.autoescape'],
 autoescape=True)
+
+
+class BrainDiagram(webapp2.RequestHandler):
+  def get(self):
+    braindiagram_html = the_jinja_env.get_template('HTML_groupproject.html')
+    self.response.write(braindiagram_html.render())
+
+
+class CssiUser(ndb.Model):
+  first_name = ndb.StringProperty()
+  last_name = ndb.StringProperty()
+  email = ndb.StringProperty()
+
+class LogIn(webapp2.RequestHandler):
+  def get(self):
+    user = users.get_current_user()
+    # If the user is logged in...
+    if user:
+      signout_link_html = '<a href="%s">sign out</a>' % (
+              users.create_logout_url('/login'))
+      signout_link = users.create_logout_url('/login')
+      email_address = user.nickname()
+      cssi_user = CssiUser.query().filter(CssiUser.email == email_address).get()
+      # If the user is registered...
+      if cssi_user:
+        # Greet them with their personal information
+            login_html = the_jinja_env.get_template('thanksforusing.html')
+            print(cssi_user)
+
+            self.response.write(login_html.render(cssi_user = cssi_user, signout_link= signout_link))
+
+      # If the user isn't registered...
+      else:
+        # Offer a registration form for a first-time visitor:
+        self.response.write('''
+            Goodbye, %s!  <br>
+            <form method="post" action="/login">
+
+            </form><br> %s <br>
+            ''' % (email_address, signout_link_html))
+    else:
+      # If the user isn't logged in...
+      login_url = users.create_login_url('/userinfo')
+      login_html = the_jinja_env.get_template('login.html')
+      # Prompt the user to sign in.
+      self.response.write(login_html.render())
+
+  def post(self):
+    user = users.get_current_user()
+    cssi_user = CssiUser(
+        first_name=self.request.get('first_name'),
+        last_name=self.request.get('last_name'),
+        email=user.nickname())
+    cssi_user.put()
+    login_html = the_jinja_env.get_template('thankssignup.html')
+    print(cssi_user)
+
+    self.response.write(login_html.render(cssi_user = cssi_user))
+
+class UserInfo(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        email_address = user.nickname()
+        cssi_user = CssiUser.query().filter(CssiUser.email == email_address).get()
+        # If the user is registered...
+        if cssi_user:
+            login_html = the_jinja_env.get_template('welcomeuser.html')
+            print(cssi_user)
+
+            self.response.write(login_html.render(cssi_user = cssi_user))
+        # If the user isn't registered...
+        else:
+          # Offer a registration form for a first-time visitor:
+          login_html = the_jinja_env.get_template('makeaccount.html')
+          self.response.write(login_html.render(cssi_user = cssi_user))
+
+
 
 
 class BrainQuizPage(webapp2.RequestHandler):
@@ -46,5 +124,8 @@ class BrainQuizPage(webapp2.RequestHandler):
         self.response.write(quiz_template.render(trivia_url_endpoint))
 
 app = webapp2.WSGIApplication([
-    ('/brainquiz', BrainQuizPage)
+    ('/brainquiz', BrainQuizPage),
+    ('/login', LogIn),
+    ('/braindiagram', BrainDiagram),
+    ('/userinfo', UserInfo),
 ], debug=True)
